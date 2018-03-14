58a863fad70c2f0452b3b48a897c805b16f1f57a
TysonParks <typarks@gmail.com>
2018-03-13 20:23:03 -0700

    Finished summing excercise and passed all tests

--- SumFromAtoB/Sums.swift
+++ SumFromAtoB/Sums.swift
@@ -1,25 +1,49 @@
-//  Copyright (c) 2017 Lighthouse Labs. All rights reserved.
-//
-// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-// distribute, sublicense, create a derivative work, and/or sell copies of the
-// Software in any work that is designed, intended, or marketed for pedagogical or
-// instructional purposes related to programming, coding, application development,
-// or information technology.  Permission for such use, copying, modification,
-// merger, publication, distribution, sublicensing, creation of derivative works,
-// or sale is expressly withheld.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
 
 import Foundation
 
 struct Sums {
+  
   func sum(from fromN: Int, to toN: Int) -> Int {
-    return -1
+    if toN - fromN > 0 {
+      return (fromN + sum(from: fromN + 1, to: toN))
+    } else {
+      return fromN
+    }
   }
 }
+
+
+
+// 4,4 = 4                    4
+// 4,5 = 4 + 5                9
+// 4,6 = 4 + 5 + 6            15
+// 4,7 = 4 + 5 + 6 + 7        22
+// 4,8 = 4 + 5 + 6 + 7 + 8    30
+
+
+// 1,1 = 1                    1
+// 1,2 = 1 + 2                3
+// 1,3 = 1 + 2 + 3            6
+// 1,4 = 1 + 2 + 3 + 4        10
+// 1,5 = 1 + 2 + 3 + 4 + 5    15
+
+// 0,1 = 0 + 1                    1
+// 0,2 = 0 + 1 + 2                3
+// 0,3 = 0 + 1 + 2 + 3            6
+// 0,4 = 0 + 1 + 2 + 3 + 4        10
+// 0,5 = 0 + 1 + 2 + 3 + 4 + 5    15
+
+// int1
+// int1 + (int2 - 0)
+// int1 + (int2 - 1) + (int2)
+
+// int1 + sum(int1 + 1 (from: int2)
+
+// (from int2 to int2 - int1)
+
+
+
+
+
+// int1 + sum(int2) - sum(int1)
+
